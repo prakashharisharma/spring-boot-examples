5c8ca0973bd98bf67380cfa6705f93dc3dc9feaa
phsharma <prakashharisharma@gmail.com>
2018-07-05 14:59:10 +0530

    20180705'

--- spring-boot-security/spring-boot-security-oauth2-jwt/oauth2-client-jwt/src/main/java/com/tutorialsdesk/config/Oauth2RestTemplateConfig.java
+++ spring-boot-security/spring-boot-security-oauth2-jwt/oauth2-client-jwt/src/main/java/com/tutorialsdesk/config/Oauth2RestTemplateConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
@@ -45,13 +46,12 @@ public class Oauth2RestTemplateConfig {
     	System.out.println(accessTokenUri);
     	System.out.println(userAuthorizationUri);
     	
-        AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
-        
+       // AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
+        ClientCredentialsResourceDetails resource = new ClientCredentialsResourceDetails();
         resource.setClientId(clientID);
         resource.setClientSecret(clientSecret);
-        //resource.setGrantType("client_credentials");
         resource.setAccessTokenUri(accessTokenUri);
-        resource.setUserAuthorizationUri(userAuthorizationUri);
+        
         resource.setScope(Arrays.asList("read"));
 
         return resource;
